3ef9cbf90bac97589af06ad118fa91cd6d8ef734
Neeraj Sharma <nsharma0619@gmail.com>
2020-05-23 12:40:15 +0530

    javascript added

---
+++ index.js
@@ -0,0 +1,47 @@
+let budget=0, totalExpenses=0, balance=0;
+let expTitle=[];
+let expAmount=[];
+
+function budgetManupulation(){
+    budget=document.getElementById("budget-data").value;
+    document.getElementById("budget").innerText=budget; 
+    balance = budget-totalExpenses;
+    document.getElementById("balance").innerText=balance;
+    if(balance<0){
+        document.getElementById("bal-icon").style.color="#cc0000";
+    }
+    else{
+        document.getElementById("bal-icon").style.color="#1f7a1f"
+    }
+    if(budget<0){
+        document.getElementById("bud-icon").style.color="#cc0000";
+    }
+    else{
+        document.getElementById("bud-icon").style.color="#1f7a1f";
+    }
+    document.getElementById("budget-data").value=""
+}
+
+function sum(ar){
+    let total=0;
+    for(i=0;i<ar.length;i++){
+        total+=Number(ar[i]);
+    }
+    return total;
+}
+
+
+function expManupulation(){
+    expTitleData=document.getElementById("expTitleData").value;
+    expAmountData=document.getElementById("expAmountData").value;
+    expTitle.push(expTitleData);
+    expAmount.push(expAmountData);
+    totalExpenses = sum(expAmount);
+    balance=budget-totalExpenses;
+    document.getElementById("balance").innerText=balance;
+    if(balance<0){
+        document.getElementById("bal-icon").style.color="#cc0000";
+    }
+    document.getElementById("expTitleData").value=""
+    document.getElementById("expAmountData").value=""
+}
